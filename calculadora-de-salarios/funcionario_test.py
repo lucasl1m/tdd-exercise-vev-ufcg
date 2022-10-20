dc27b937eb9b1949e0aa3e6b384badc31f6e2de3
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-10-20 08:34:08 -0300

    feat: add test `funcionario_test.py`

---
+++ calculadora-de-salarios/funcionario_test.py
@@ -0,0 +1,8 @@
+def test_funcionario():
+    funcionario = Funcionario()
+    assert funcionario.nome == ''
+    assert funcionario.email == ''
+    assert funcionario.salario_base == 0
+    assert funcionario.cargo == ''
+    
+
