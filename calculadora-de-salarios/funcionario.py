bb5ba2cd0de362e67ce1b48e0208c83575277136
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-10-20 13:53:19 -0300

    refactor: only accept valid positions in `funcionario.py`

--- calculadora-de-salarios/funcionario.py
+++ calculadora-de-salarios/funcionario.py
@@ -2,8 +2,8 @@ class Funcionario:
     def __init__(self, nome, email, salario_base, cargo):
         self.nome = nome
         self.email = email
-        self.salario_base = salario_base
-        self.cargo = cargo
+        self.salario_base = salario_base 
+        self.cargo = cargo.upper() if cargo.upper() in ['DESENVOLVEDOR', 'DBA', 'TESTADOR', 'GERENTE'] else ''
     
     def get_salario(self):
         return self.salario_base
--- calculadora-de-salarios/funcionario_test.py
+++ calculadora-de-salarios/funcionario_test.py
@@ -1,5 +1,6 @@
 from funcionario import *
 
+# Teste inicialização da classe Funcionario
 def test_funcionario_init():
     funcionario = Funcionario('', '', 0, '')
     assert funcionario.nome == ''
@@ -13,4 +14,16 @@ def test_get_salario():
 
 def test_get_cargo():
     funcionario = Funcionario('', '', 0, '')
+    assert funcionario.get_cargo() == ''
+
+    # Teste valor de cargo válido
+    funcionario = Funcionario('', '', 0, 'DESENVOLVEDOR')
+    assert funcionario.get_cargo() == 'DESENVOLVEDOR'
+
+    # Teste valor de cargo captializado ou minúsculo
+    funcionario = Funcionario('', '', 0, 'Gerente')
+    assert funcionario.get_cargo() == 'GERENTE'
+
+    # Teste valor de cargo inválido
+    funcionario = Funcionario('', '', 0, 'PO')
     assert funcionario.get_cargo() == ''
