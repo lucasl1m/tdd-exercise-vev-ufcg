054a5941262dbd0f57820c8ebc0bb3f278fa7dfc
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-10-21 11:35:30 -0300

    refactor: only accept valid salary in `funcionario.py`

--- calculadora-de-salarios/funcionario.py
+++ calculadora-de-salarios/funcionario.py
@@ -2,7 +2,7 @@ class Funcionario:
     def __init__(self, nome, email, salario_base, cargo):
         self.nome = nome
         self.email = email
-        self.salario_base = salario_base 
+        self.salario_base = salario_base if salario_base >= 0 else 0 
         self.cargo = cargo.upper() if cargo.upper() in ['DESENVOLVEDOR', 'DBA', 'TESTADOR', 'GERENTE'] else ''
     
     def get_salario(self):
--- calculadora-de-salarios/funcionario_test.py
+++ calculadora-de-salarios/funcionario_test.py
@@ -1,29 +1,34 @@
+import pytest
 from funcionario import *
 
-# Teste inicialização da classe Funcionario
-def test_funcionario_init():
+def test_valid_params_funcionario_init():
     funcionario = Funcionario('', '', 0, '')
     assert funcionario.nome == ''
     assert funcionario.email == ''
     assert funcionario.salario_base == 0
     assert funcionario.cargo == ''
 
-def test_get_salario():
+def test_invalid_params_funcionario_init():
+  with pytest.raises(TypeError):
+    funcionario = Funcionario()
+    funcionario = Funcionario('', '', 0 )
     funcionario = Funcionario('', '', 0, '')
+
+def test_invalid_params_get_salario():
+    funcionario = Funcionario('', '', -1, '')
     assert funcionario.get_salario() == 0
 
-def test_get_cargo():
+def test_invalid_params_get_cargo():
     funcionario = Funcionario('', '', 0, '')
     assert funcionario.get_cargo() == ''
 
-    # Teste valor de cargo válido
+    funcionario = Funcionario('', '', 0, 'PO')
+    assert funcionario.get_cargo() == ''
+
+def test_valid_params_get_cargo():
     funcionario = Funcionario('', '', 0, 'DESENVOLVEDOR')
     assert funcionario.get_cargo() == 'DESENVOLVEDOR'
 
-    # Teste valor de cargo captializado ou minúsculo
     funcionario = Funcionario('', '', 0, 'Gerente')
     assert funcionario.get_cargo() == 'GERENTE'
 
-    # Teste valor de cargo inválido
-    funcionario = Funcionario('', '', 0, 'PO')
-    assert funcionario.get_cargo() == ''
