3551d76a6de7daac29c6783a73ff89cf46bac7c3
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-10-21 12:09:35 -0300

    refactor: test_valid_calc_salario_liquido() in `funcionario_test.py`

--- calculadora-de-salarios/funcionario_test.py
+++ calculadora-de-salarios/funcionario_test.py
@@ -34,25 +34,25 @@ def test_valid_params_get_cargo():
 
 def test_valid_calc_salario_liquido():
     funcionario = Funcionario('', '', 7000, 'DESENVOLVEDOR')
-    assert funcionario.calc_salario_liquido() == 5600
+    assert funcionario.calc_salario_liquido() == 5600.00
 
     funcionario = Funcionario('', '', 2900, 'DESENVOLVEDOR')
-    assert funcionario.calc_salario_liquido() == 2610
+    assert funcionario.calc_salario_liquido() == 2610.00
 
     funcionario = Funcionario('', '', 2000, 'DBA')
-    assert funcionario.calc_salario_liquido() == 1500
+    assert funcionario.calc_salario_liquido() == 1500.00
 
     funcionario = Funcionario('', '', 1900, 'DBA')
-    assert funcionario.calc_salario_liquido() == 1615
+    assert funcionario.calc_salario_liquido() == 1615.00
 
     funcionario = Funcionario('', '', 3000, 'TESTADOR')
-    assert funcionario.calc_salario_liquido() == 2250
+    assert funcionario.calc_salario_liquido() == 2250.00
 
     funcionario = Funcionario('', '', 1999, 'TESTADOR')
     assert funcionario.calc_salario_liquido() == 1699.15
 
     funcionario = Funcionario('', '', 4000, 'GERENTE')
-    assert funcionario.calc_salario_liquido() == 3200
+    assert funcionario.calc_salario_liquido() == 3200.00
 
     funcionario = Funcionario('', '', 5000, 'GERENTE')
-    assert funcionario.calc_salario_liquido() == 3800
+    assert funcionario.calc_salario_liquido() == 3500.00
