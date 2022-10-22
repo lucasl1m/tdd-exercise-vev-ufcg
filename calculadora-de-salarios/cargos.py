34145dc341d7e32abd9fbef93efe8eb87361d0bc
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-10-22 10:50:27 -0300

    refactor: methods and constructor in `funcionario.py``

---
+++ calculadora-de-salarios/cargos.py
@@ -0,0 +1,22 @@
+cargos = {
+    'DESENVOLVEDOR': {
+        'limite': 3000,
+        'desconto1': 0.20,
+        'desconto2': 0.10
+    },
+    'DBA': {
+        'limite': 2000,
+        'desconto1': 0.25,
+        'desconto2': 0.15
+    },
+    'TESTADOR': {
+        'limite': 2000,
+        'desconto1': 0.25,
+        'desconto2': 0.15
+    },
+    'GERENTE': {
+        'limite': 5000,
+        'desconto1': 0.30,
+        'desconto2': 0.20
+    }
+}
--- calculadora-de-salarios/funcionario.py
+++ calculadora-de-salarios/funcionario.py
@@ -1,9 +1,17 @@
+from cargos import cargos
+
 class Funcionario:
     def __init__(self, nome, email, salario_base, cargo):
-        self.nome = nome
-        self.email = email
-        self.salario_base = salario_base if salario_base >= 0 else 0 
-        self.cargo = cargo.upper() if cargo.upper() in ['DESENVOLVEDOR', 'DBA', 'TESTADOR', 'GERENTE'] else ''
+        try:
+            if (nome == '' or email == '' or salario_base < 0 or cargo == '' or cargo.upper() not in cargos):
+                raise ValueError
+            else:
+                self.nome = nome
+                self.email = email
+                self.salario_base = salario_base
+                self.cargo = cargo.upper()
+        except ValueError:
+            raise ValueError('Invalid parameters')
     
     def get_salario(self):
         return self.salario_base
@@ -12,25 +20,8 @@ class Funcionario:
         return self.cargo
     
     def calc_salario_liquido(self):
-        if self.cargo == 'DESENVOLVEDOR':
-            if self.salario_base >= 3000:
-                return float(f'{self.salario_base * 0.8:.2f}')
-            else:
-                return float(f'{self.salario_base * 0.9:.2f}')
-        elif self.cargo == 'DBA':
-            if self.salario_base >= 2000:
-                return float(f'{self.salario_base * 0.75:.2f}')
-            else:
-                return float(f'{self.salario_base * 0.85:.2f}')
-        elif self.cargo == 'TESTADOR':
-            if self.salario_base >= 2000:
-                return float(f'{self.salario_base * 0.75:.2f}')
-            else:
-                return float(f'{self.salario_base * 0.85:.2f}')
-        elif self.cargo == 'GERENTE':
-            if self.salario_base >= 5000:
-                return float(f'{self.salario_base * 0.7:.2f}')
-            else:
-                return float(f'{self.salario_base * 0.8:.2f}')
+
+        if ( cargos[self.cargo.upper()]['limite'] <= self.salario_base ):
+            return self.salario_base - (self.salario_base * (cargos[self.cargo.upper()]['desconto1']))
         else:
-            return 0
+            return self.salario_base - (self.salario_base * (cargos[self.cargo.upper()]['desconto2']))
