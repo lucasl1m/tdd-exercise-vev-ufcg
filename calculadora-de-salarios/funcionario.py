559a7dd363121b2832e1efc1559d266bad1d6141
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-10-22 13:19:17 -0300

    refactor: simplification of the init method

--- calculadora-de-salarios/funcionario.py
+++ calculadora-de-salarios/funcionario.py
@@ -2,16 +2,14 @@ from cargos import cargos
 
 class Funcionario:
     def __init__(self, nome, email, salario_base, cargo):
-        try:
-            if (nome == '' or email == '' or salario_base < 0 or cargo == '' or cargo.upper() not in cargos):
-                raise ValueError
-            else:
-                self.nome = nome
-                self.email = email
-                self.salario_base = salario_base
-                self.cargo = cargo.upper()
-        except ValueError:
-            raise ValueError('Invalid parameters')
+        if (nome == '' or email == '' or salario_base < 0 or cargo == '' or cargo.upper() not in cargos):
+            raise ValueError( 'Invalid parameters' )
+
+        self.nome = nome
+        self.email = email
+        self.salario_base = salario_base
+        self.cargo = cargo.upper()
+        
     
     def get_salario(self):
         return self.salario_base
@@ -21,7 +19,7 @@ class Funcionario:
     
     def calc_salario_liquido(self):
 
-        if ( cargos[self.cargo.upper()]['limite'] <= self.salario_base ):
-            return self.salario_base - (self.salario_base * (cargos[self.cargo.upper()]['desconto1']))
+        if ( cargos[self.cargo]['limite'] <= self.salario_base ):
+            return self.salario_base - (self.salario_base * (cargos[self.cargo]['desconto1']))
         else:
-            return self.salario_base - (self.salario_base * (cargos[self.cargo.upper()]['desconto2']))
+            return self.salario_base - (self.salario_base * (cargos[self.cargo]['desconto2']))
